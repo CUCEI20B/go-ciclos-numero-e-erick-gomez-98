e7b0fef169367e564505b0c2d0e6e076ffbefd8e
Erick Gómez <erick.gomez4187@alumnos.udg.mx>
2020-09-30 18:42:12 -0500

    Se terminío la actividad

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var limite int
+	fmt.Scanln(&limite)
+	var out float64
+	if limite == 0 {
+		out = 1
+	} else {
+		for i := 1; i <= limite; i++ {
+			out += float64((1.0 / float64(factorial(i))))
+		}
+	}
+	fmt.Println(1.0 + out)
+}
+
+func factorial(n int) uint64 {
+	var f uint64 = 1
+
+	for i := 1; i <= n; i++ {
+		f *= uint64(i)
+	}
+
+	return f
 }
